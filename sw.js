87214476b76ba4014587124575ff51701282567d
Rosh <roshjingel@gmail.com>
2025-11-01 15:25:10 +0800

    UPDATED

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
 const CACHE_NAME = 'offline-cache-v1';
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js'
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js'
 ];
 
 // Install service worker
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
 });
 
 // Activate service worker
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+    event.waitUntil(self.clients.claim());
 });
 
 // Fetch cached files when offline
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
 });
